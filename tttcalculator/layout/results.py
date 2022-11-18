add power for number of remaining riders, fixes for stdev calculation to match excel, change some menu item names

tttcalculator/layout/results.py
@@ -15,6 +15,7 @@ def results_layout(
     reference_power_scale: int,
     target_turn_length: int,
     pleasure_index: PleasureIndex,
+    power_multiplier_data: pd.DataFrame,
 ):
 
     _validate_metrics(riders, reference_metric)
@@ -31,9 +32,14 @@ def results_layout(
     stats = riders.stats(ref_metric_name)
     turn_lengths = riders.get_turn_lengths(reference_metric, target_turn_length, reference_power_scale)
 
-    pleasure_factor = pleasure_index.value[2]
-    power_min = pleasure_factor * stats["average"]
-    power_max = pleasure_factor * stats["sec_average"]
+    # read the power multipluers csv and get the right column for the pleasure index
+    power_multipliers = power_multiplier_data[f"{pleasure_index.value[0]}"].values
+    # only keep the power multipliers for the number of riders
+    power_multipliers = power_multipliers[
+        len(power_multiplier_data) - len(riders) : len(power_multiplier_data)  # noqa: E203
+    ]
+    power_max = stats["average"] * power_multipliers
+    power_min = stats["sec_average"] * power_multipliers
 
     cols = st.columns(2)
 
@@ -77,6 +83,9 @@ def _turn_lengths(
     st=st,
 ):
 
+    # round turn lengths to nearest 5 seconds
+    turn_lengths = [int(round(turn_length / 5) * 5) for turn_length in turn_lengths]
+
     st.header("Turn Lengths")
     turn_length_data = defaultdict(list)
     for rider, turn_length in zip(riders, turn_lengths):
@@ -86,6 +95,7 @@ def _turn_lengths(
 
     df = pd.DataFrame(turn_length_data)
     df = df.set_index("Number")
+    st.write("Rider number:")
     st.table(df)
 
 
@@ -94,6 +104,8 @@ def _stats(stats: dict[str, float], float_formatter: Callable, turn_lengths: lis
     st.header("Stats")
 
     st.write(f"Average power: {float_formatter(stats['average'])} {power_unit}")
+    st.write(f"St Dev: {float_formatter(stats['stddev'])} {power_unit}")
+    st.write(f"Second average power: {float_formatter(stats['sec_average'])} {power_unit}")
     st.write(f"Average turn length: {np.average(turn_lengths):.0f} seconds")
 
     rest_time_seconds = np.sum(turn_lengths) - np.average(turn_lengths)
@@ -109,5 +121,18 @@ def _target_power(
     st=st,
 ):
     st.header(f"Target {power_unit}")
-    st.write(f"Min: {float_formatter(power_min)}")
-    st.write(f"Max: {float_formatter(power_max)}")
+
+    number_riders = range(len(power_min), 0, -1)
+    power_min_str = [float_formatter(power) for power in power_min]
+    power_max_str = [float_formatter(power) for power in power_max]
+
+    df = pd.DataFrame(
+        {
+            "# Riders": number_riders,
+            "Min": power_min_str,
+            "Max": power_max_str,
+        },
+    )
+    st.write("Riders remaining:")
+    df = df.set_index("# Riders")
+    st.table(df)

tttcalculator/riders.py
@@ -132,7 +132,9 @@ class Riders:
 
         att_values = self.get_rider_attribute(attr)
 
-        return np.std(att_values)
+        return (
+            np.std(att_values, ddof=1) / 2
+        )  # ddof=1 for sample stddev (match excel), /2 matches the TTT original calculator
 
     def sec_average(self, attr: str):
 