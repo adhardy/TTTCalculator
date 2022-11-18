6c66c1cbab326093cddf02aaaf96446a83f97878
Adam <adam.hardy@deepmatter.io>
2022-11-18 21:15:46 +0000

    enhance dropped riders power display

--- tttcalculator/layout/results.py
+++ tttcalculator/layout/results.py
@@ -32,26 +32,20 @@ def results_layout(
     stats = riders.stats(ref_metric_name)
     turn_lengths = riders.get_turn_lengths(reference_metric, target_turn_length, reference_power_scale)
 
-    # read the power multipluers csv and get the right column for the pleasure index
-    power_multipliers = power_multiplier_data[f"{pleasure_index.value[0]}"].values
-    # only keep the power multipliers for the number of riders
-    power_multipliers = power_multipliers[
-        len(power_multiplier_data) - len(riders) : len(power_multiplier_data)  # noqa: E203
-    ]
-    power_max = stats["average"] * power_multipliers
-    power_min = stats["sec_average"] * power_multipliers
-
     cols = st.columns(2)
 
     _target_power(
-        power_min,
-        power_max,
+        power_multiplier_data,
+        pleasure_index,
+        len(riders),
         float_formatter,
         power_unit,
+        stats["average"],
+        stats["sec_average"],
         cols[0],
     )
 
-    _turn_lengths(turn_lengths, riders, cols[1])
+    _turn_lengths(turn_lengths, riders, float_formatter, reference_metric, power_unit, cols[1])
 
     _stats(
         stats,
@@ -80,6 +74,9 @@ def _validate_metrics(riders: Riders, reference_metric: ReferenceMetric):
 def _turn_lengths(
     turn_lengths: list[float],
     riders: Riders,
+    float_formatter: Callable,
+    reference_metric: ReferenceMetric,
+    power_unit: str,
     st=st,
 ):
     st.header("Turn Lengths")
@@ -93,10 +90,13 @@ def _turn_lengths(
         turn_length_data["Number"].append(rider.number + 1)
         turn_length_data["Rider"].append(rider.name)
         turn_length_data["Turn Length"].append(int(turn_length))
+        turn_length_data[f"40 min. Power ({power_unit})"].append(
+            float_formatter(getattr(rider, reference_metric.name.lower()))
+        )
 
     df = pd.DataFrame(turn_length_data)
     df = df.set_index("Number")
-    st.write("Rider number:")
+    st.caption("Rider number:")
     st.table(df)
 
 
@@ -104,37 +104,44 @@ def _stats(stats: dict[str, float], float_formatter: Callable, turn_lengths: lis
 
     st.header("Stats")
 
-    st.write(f"Average power: {float_formatter(stats['average'])} {power_unit}")
-    st.write(f"St Dev: {float_formatter(stats['stddev'])} {power_unit}")
-    st.write(f"Second average power: {float_formatter(stats['sec_average'])} {power_unit}")
     st.write(f"Average turn length: {np.average(turn_lengths):.0f} seconds")
 
     rest_time_seconds = np.sum(turn_lengths) - np.average(turn_lengths)
     rest_time_str = f"{int(rest_time_seconds / 60)}min {int(rest_time_seconds % 60)}s"
     st.write(f"Rest time: {rest_time_str}")
 
+    st.write(f"Average power: {float_formatter(stats['average'])} {power_unit}")
+
 
 def _target_power(
-    power_min: float,
-    power_max: float,
+    power_multiplier_data: pd.DataFrame,
+    pleasure_index: PleasureIndex,
+    num_riders: int,
     float_formatter: Callable,
     power_unit: str,
+    average_power: float,
+    second_average_power: float,
     st=st,
 ):
     st.header(f"Target {power_unit}")
     st.caption("Target power for the rider at the front of the group. As riders drop off, target power decreases.")
 
-    number_riders = range(len(power_min), 0, -1)
-    power_min_str = [float_formatter(power) for power in power_min]
-    power_max_str = [float_formatter(power) for power in power_max]
+    # read the power multipluers csv and get the right column for the pleasure index
+    power_multipliers = power_multiplier_data[f"{pleasure_index.value[0]}"]
+    # select only rows where rider number is less than or equal to the number of riders
+    power_multipliers = power_multipliers[power_multipliers.index <= num_riders]
+    s_power_max = average_power * power_multipliers
+    s_power_max.name = "Max"
+    s_power_max = s_power_max.apply(float_formatter)
+    s_power_min = second_average_power * power_multipliers
+    s_power_min.name = "Min"
+    s_power_min = s_power_min.apply(float_formatter)
 
-    df = pd.DataFrame(
-        {
-            "# Riders": number_riders,
-            "Min": power_min_str,
-            "Max": power_max_str,
-        },
+    df_power = pd.merge(
+        s_power_max,
+        s_power_min,
+        left_index=True,
+        right_index=True,
     )
-    st.write("Riders remaining:")
-    df = df.set_index("# Riders")
-    st.table(df)
+    st.caption("Riders remaining:")
+    st.table(df_power)
